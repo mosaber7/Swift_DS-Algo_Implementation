2d28435fc77630d07732782b6a80ed0a777f8ba8
MoSaber <msaberms72016@gmail.com>
2021-07-30 20:30:47 +0200

    added Stack implemntation

--- Pages/Stacks.xcplaygroundpage/Contents.swift
+++ Pages/Stacks.xcplaygroundpage/Contents.swift
@@ -1,7 +1,27 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var str = "Hello, playground"
-
-//: [Next](@next)
+public struct Stack<Element>{
+    
+    private var storage: [Element] = []
+    
+    
+    public mutating func push(_ element: Element){
+        storage.append(element)
+    }
+    @discardableResult
+    public mutating func pop()-> Element? {
+        storage.popLast()
+    }
+    
+    public func peak()-> Element{
+        storage[storage.count-1]
+    }
+    var isEmpty: Bool{
+        storage.isEmpty //peak() == nil
+    }
+}
+// make intialization with array literal possible[]
+// var stack :Stack<> = []
+extension Stack: ExpressibleByArrayLiteral{
+    public init(arrayLiteral elements: Element...){
+        storage = elements
+    }
+}
